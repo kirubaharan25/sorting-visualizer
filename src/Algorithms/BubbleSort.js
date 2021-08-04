61a9651a90225e14dcea52beb1ee0e2ddde80065
kirubaharan25 <74064577+kirubaharan25@users.noreply.github.com>
2021-08-04 15:58:19 +0100

    Update BubbleSort.js

---
+++ src/Algorithms/BubbleSort.js
@@ -0,0 +1,17 @@
+
+
+export default function bubbleSort() {
+    var array = [2,3,4];
+    var len = array.length;
+
+    for(let i=0; i < len;i++) {
+        for(let j=0; j< len;j++) {
+            if(array[i] > array[j+1]) {
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    return array;
+};
